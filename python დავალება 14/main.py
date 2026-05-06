0d5703135e6b2cefe0dcf031f1810b1e5de08634
lagvilava2811 <your-email@example.com>
2026-05-06 15:31:19 +0400

    Add python davaleba 14

---
+++ python დავალება 14/main.py
@@ -0,0 +1,49 @@
+# ================== დავალება ===============================
+
+class Student:
+    status = True
+
+    def __init__(self, first_name, last_name, age, grades):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.grades = grades
+        self.pay = 1000
+
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+
+    def get_discount(self):
+        if self.age < 18:
+            self.pay = self.pay * 0.8
+        return self.pay
+
+    def calculate_average(self):
+        total = 0
+        for grade in self.grades:
+            total = total + grade
+        return total / len(self.grades)
+
+    def get_status(self):
+        avg = self.calculate_average()
+        
+        if avg > 90:
+            return "Excellent"
+        if avg >= 70:
+            return "Good"
+        if avg >= 50:
+            return "Average"
+        
+        Student.status = False
+        return "Poor"
+
+
+#=================== ტესტი ===================
+s1 = Student("vako", "lagvilava", 35, [95, 88, 92, 78, 85])
+s2 = Student("Nino", "abazadze", 16, [45, 38, 52, 41, 47])
+s3 = Student("Mariam", "abashidze", 17, [55, 58, 52, 60, 54])
+
+print(s1.get_full_name(), s1.get_status(), s1.get_discount())
+print(s2.get_full_name(), s2.get_status(), s2.get_discount())
+print(s3.get_full_name(), s3.get_status(), s3.get_discount())
+print("Class status:", Student.status)
