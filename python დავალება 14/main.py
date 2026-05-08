6b224a5ec63777b06a58dcbc0a691b5cbef6886c
lagvilava2811 <your-email@example.com>
2026-05-08 22:53:35 +0400

    Update main.py

--- python დავალება 14/main.py
+++ python დავალება 14/main.py
@@ -1,14 +1,15 @@
 # ================== დავალება ===============================
 
 class Student:
-    status = True
+    status = True  
+    pay = 1000
 
     def __init__(self, first_name, last_name, age, grades):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.grades = grades
-        self.pay = 1000
+        self.status = True  
 
     def get_full_name(self):
         return self.first_name + " " + self.last_name
@@ -26,24 +27,27 @@ class Student:
 
     def get_status(self):
         avg = self.calculate_average()
-        
+
         if avg > 90:
             return "Excellent"
         if avg >= 70:
             return "Good"
         if avg >= 50:
             return "Average"
-        
-        Student.status = False
+
+        self.status = False
         return "Poor"
 
 
-#=================== ტესტი ===================
-s1 = Student("vako", "lagvilava", 35, [95, 88, 92, 78, 85])
-s2 = Student("Nino", "abazadze", 16, [45, 38, 52, 41, 47])
-s3 = Student("Mariam", "abashidze", 17, [55, 58, 52, 60, 54])
+s1 = Student("Vako", "Lagvilava", 35, [95, 88, 92, 78, 85])
+s2 = Student("Nino", "Abazadze", 16, [45, 38, 52, 41, 47])
+s3 = Student("Mariam", "Abashidze", 17, [55, 58, 52, 60, 54])
 
 print(s1.get_full_name(), s1.get_status(), s1.get_discount())
 print(s2.get_full_name(), s2.get_status(), s2.get_discount())
 print(s3.get_full_name(), s3.get_status(), s3.get_discount())
-print("Class status:", Student.status)
+
+print("\nAll students status:")
+print("s1.status:", s1.status)
+print("s2.status:", s2.status)
+print("s3.status:", s3.status)
